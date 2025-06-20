8a2203470b0b75ed45a9d997b4aad83c511ccd95
Nadev <120123131+Nadee23@users.noreply.github.com>
2025-06-20 18:24:39 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,3 +1,66 @@
 //write your code here
-console.log("Hello Rigo from the console!");
+const posibilities = ["Paper", "Scissors", "Rock", "Lizard", "Spock"]
+const AIChoice = posibilities[Math.floor(Math.random() * posibilities.length)]
 
+function GameLogic(Choose) {
+
+    console.log("User chose: " + Choose);
+    console.log("Computer chose: " + AIChoice);
+
+    if (AIChoice === Choose) {
+        return "It´s a draw!"
+
+    }
+
+    if (Choose === "Rock") {
+        if (AIChoice === "Scissors") {
+            return "You win! 🎉"
+        }
+        if (AIChoice === "Lizard") {
+            return "You win! 🎉"
+        }
+    }
+
+    if (Choose === "Paper") {
+        if (AIChoice === "Rock") {
+            return "You win! 🎉"
+        }
+        if (AIChoice === "Spock") {
+            return "You win! 🎉"
+        }
+    }
+
+    if (Choose === "Scissors") {
+        if (AIChoice === "Paper") {
+            return "You win! 🎉"
+        }
+        if (AIChoice === "Lizard") {
+            return "You win! 🎉"
+        }
+    }
+
+    if (Choose === "Lizard") {
+        if (AIChoice === "Paper") {
+            return "You win! 🎉"
+        }
+        if (AIChoice === "Spock") {
+            return "You win! 🎉"
+        }
+    }
+
+    if (Choose === "Spock") {
+        if (AIChoice === "Scissors") {
+            return "You win! 🎉"
+        }
+        if (AIChoice === "Rock") {
+            return "You win! 🎉"
+        }
+    }
+    return "You lose 😢"
+}
+console.log(posibilities);
+console.log(AIChoice);
+
+let answer = prompt("Write 1 of the next options to play: Scissors, Rock, Paper, Lizard, Spock:");
+
+console.log(GameLogic(answer));
