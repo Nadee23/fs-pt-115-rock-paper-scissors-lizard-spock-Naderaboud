96d3e0f72c63aa9ac8e67bc7140ec3d28ee0a868
Nadev <120123131+Nadee23@users.noreply.github.com>
2025-06-25 18:35:27 +0000

    fixed

--- src/app.js
+++ src/app.js
@@ -7,36 +7,37 @@ function GameLogic(playerChoose) {
     console.log("User chose: " + playerChoose);
     console.log("Computer chose: " + AIChoice);
 
+    // draw condition
     if (AIChoice === playerChoose) {
         return "It´s a draw!"
 
     }
-
+    //if player choose rock win conditions
     if (playerChoose === "Rock" && (AIChoice === "Scissors" || AIChoice === "Lizard")) {
         return "You win! 🎉"
 
     }
-
+    //if player choose paper win conditions
     if (playerChoose === "Paper" && (AIChoice === "Rock" || AIChoice === "Spock")) {
         return "You win! 🎉"
 
     }
-
+    //if player choose scissors win conditions
     if (playerChoose === "Scissors" && (AIChoice === "Paper" || AIChoice === "Lizard")) {
         return "You win! 🎉"
 
     }
-
+    //if player choose lizard win conditions
     if (playerChoose === "Lizard" && (AIChoice === "Paper" || AIChoice === "Spock")) {
         return "You win! 🎉"
 
     }
-
+    //if player choose Spock win conditions
     if (playerChoose === "Spock" && (AIChoice === "Scissors" || AIChoice === "Rock")) {
         return "You win! 🎉"
 
     }
-
+    //if player doesn´t wins, he lose.
     return "You lose 😢"
 }
 
